2043b646123ff630c53f5e8027a01e313c921f60
Christian Yao Komenan <kycdev@gmail.com>
2019-09-13 00:41:41 +0000

    commit

--- usermanagement-client-deployment/script/docker/push.bat
+++ usermanagement-client-deployment/script/docker/push.bat
@@ -1,2 +1,2 @@
-docker tag mic-utilisateur-gestion-client 10.3.4.18:5000/mic-utilisateur-gestion-client:201908080733
-docker push 10.3.4.18:5000/mic-utilisateur-gestion-client:201908080733
+docker tag mic-utilisateur-gestion-client 10.3.4.18:5000/mic-utilisateur-gestion-client:201909031550
+docker push 10.3.4.18:5000/mic-utilisateur-gestion-client:201909031550
