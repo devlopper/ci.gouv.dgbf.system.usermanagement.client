809c3705781c738f0fbf24a9d453ecf540f3f333
Christian Yao Komenan <kycdev@gmail.com>
2019-11-07 20:20:31 +0000

    commit

--- usermanagement-client-deployment/script/docker/push.bat
+++ usermanagement-client-deployment/script/docker/push.bat
@@ -1,2 +1,7 @@
-docker tag mic-utilisateur-gestion-client 10.3.4.18:5000/mic-utilisateur-gestion-client:201910181515
-docker push 10.3.4.18:5000/mic-utilisateur-gestion-client:201910181515
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+SET timestamp=%mydate%%mytime%
+echo Publishing with time stamp : %timestamp%
+docker tag mic-utilisateur-gestion-client 10.3.4.18:5000/mic-utilisateur-gestion-client:%timestamp%
+docker push 10.3.4.18:5000/mic-utilisateur-gestion-client:%timestamp%
